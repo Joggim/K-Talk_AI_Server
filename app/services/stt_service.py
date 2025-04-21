8ceb253e91210d4022a4abbf3370ef5306ae74bb
haeun228 <123haeun@naver.com>
2025-04-21 16:05:14 +0900

    Feat: Whisper STT 추가

--- app/services/stt_service.py
+++ app/services/stt_service.py
@@ -1,18 +1,23 @@
-import shutil
 import os
+import shutil
+import tempfile
 
 import torch
 import soundfile as sf
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+import whisper
 from fastapi import HTTPException, UploadFile
-from pydub import AudioSegment  # 추가
-import tempfile
+from pydub import AudioSegment
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 
-# 모델 및 프로세서 초기화 (전역 로딩)
-model_name = "kresnik/wav2vec2-large-xlsr-korean"
-processor = Wav2Vec2Processor.from_pretrained(model_name)
-model = Wav2Vec2ForCTC.from_pretrained(model_name)
-model.eval()
+# Wav2Vec2 모델 및 프로세서 초기화
+wav2vec_model_name = "kresnik/wav2vec2-large-xlsr-korean"
+wav2vec_processor = Wav2Vec2Processor.from_pretrained(wav2vec_model_name)
+wav2vec_model = Wav2Vec2ForCTC.from_pretrained(wav2vec_model_name)
+wav2vec_model.eval()
+
+# Whisper 모델 초기화
+whisper_model = whisper.load_model("base")
+
 
 async def transcribe_audio_file_wav2vec(file: UploadFile) -> str:
     try:
@@ -29,12 +34,11 @@ async def transcribe_audio_file_wav2vec(file: UploadFile) -> str:
 
         # 3. 오디오 읽기
         audio_input, sr = sf.read(temp_wav_path)
-
         if len(audio_input.shape) > 1:
             audio_input = audio_input.mean(axis=1)
 
         # 4. 모델 입력
-        input_values = processor(
+        input_values = wav2vec_processor(
             audio_input,
             sampling_rate=16000,
             return_tensors="pt",
@@ -42,10 +46,10 @@ async def transcribe_audio_file_wav2vec(file: UploadFile) -> str:
         ).input_values
 
         with torch.no_grad():
-            logits = model(input_values).logits
+            logits = wav2vec_model(input_values).logits
         predicted_ids = torch.argmax(logits, dim=-1)
 
-        transcription = processor.batch_decode(predicted_ids)
+        transcription = wav2vec_processor.batch_decode(predicted_ids)
         return transcription[0]
 
     finally:
@@ -53,3 +57,23 @@ async def transcribe_audio_file_wav2vec(file: UploadFile) -> str:
         for path in [original_path, temp_wav_path]:
             if os.path.exists(path):
                 os.remove(path)
+
+
+async def transcribe_audio_file_whisper(file: UploadFile) -> str:
+    temp_file_path = "temp_audio.wav"
+
+    # 파일 저장
+    with open(temp_file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    try:
+        # STT 수행 (언어 명시!)
+        result = whisper_model.transcribe(
+            temp_file_path,
+            temperature=0.0,
+            language="ko"
+        )
+        return result["text"]
+    finally:
+        # 임시 파일 삭제
+        os.remove(temp_file_path)
