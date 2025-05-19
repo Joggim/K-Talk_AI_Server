95c16844d1439ce75d65cbd57b4ca1d8ad1e7833
haeun228 <123haeun@naver.com>
2025-05-19 15:03:36 +0900

    Fix: 모델 경로 오류 수정

--- app/services/predict_error_service.py
+++ app/services/predict_error_service.py
@@ -1,6 +1,11 @@
+import os
 import torch
 import torch.nn as nn
 import joblib
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "../models/mlp_pronunciation_classifier.pt")
+ENCODER_PATH = os.path.join(BASE_DIR, "../models/label_encoder.pkl")
 
 # 🔡 자모 인코딩 함수
 def encode_jamo(jamo):
@@ -36,10 +41,10 @@ HIDDEN_DIM = 128
 OUTPUT_DIM = 7
 
 model = MLPClassifier(VOCAB_SIZE, EMBED_DIM, HIDDEN_DIM, OUTPUT_DIM)
-model.load_state_dict(torch.load("/Users/seoyun/Desktop/K-Talk_AI_Server/app/models/mlp_pronunciation_classifier.pt"))
+model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device("cpu")))
 model.eval()
 
-label_encoder = joblib.load("/Users/seoyun/Desktop/K-Talk_AI_Server/app/models/label_encoder.pkl")
+label_encoder = joblib.load(ENCODER_PATH)
 
 def predict_error_type(target, user, prev, next_, jamo_index_in_syllable):
     encoded = torch.tensor([[
@@ -52,4 +57,3 @@ def predict_error_type(target, user, prev, next_, jamo_index_in_syllable):
     with torch.no_grad():
         output = model(encoded)
         pred_idx = output.argmax(1).item()
-
